18a93786361cac60b5818391157af9938144d81b
William Goi <williamalveslp@hotmail.com>
2021-03-19 11:32:31 -0300

    Created MSTest project.

---
+++ Project/Backend_.NetCore_3_1/StefaniniCore.MSTest.Tests/ServicesTests/SampleTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StefaniniCore.MSTest.Tests.ServicesTests
+{
+    [TestClass]
+    public class SampleTest
+    {
+        [TestMethod]
+        public void SumNumbers()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
